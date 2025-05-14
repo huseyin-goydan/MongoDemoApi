00569300c938f17c875a21db2d2e0c7787eda375
Hüseyin Göydan <huseyin.goydan@istikbal.com.tr>
2025-05-14 08:27:00 +0300

    Deneme 1 | Deneme vs eklentisi

--- MongoDemoApi/Program.cs
+++ MongoDemoApi/Program.cs
@@ -11,6 +11,7 @@ builder.Services.Configure<MongoDatabaseSettings>(
 // ProductService servisini ekle
 builder.Services.AddSingleton<ProductService>();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
