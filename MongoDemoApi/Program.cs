2dbbbbb3418e217a5ee6a39301fb52c06aaa89c7
Hüseyin Göydan <huseyin.goydan@istikbal.com.tr>
2025-05-14 08:40:14 +0300

    Master 2 | Master 2 deneme

--- MongoDemoApi/Program.cs
+++ MongoDemoApi/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add CORS policy
+// Add CORS policy 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
